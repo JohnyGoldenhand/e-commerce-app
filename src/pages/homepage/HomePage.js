459638646d3fb9c8d4337850a235751646d9b742
Remigiusz Mikolajewicz <r.mikolajewicz@interia.pl>
2022-01-31 00:52:32 +0100

    repleaced hard coded menu item component with actual component and added dynamic style to it

---
+++ src/pages/homepage/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './HomePage.scss';
+import Directory from '../../components/directory/directory';
+
+const HomePage = () => (
+         <div className='home-page'>
+            <Directory/>
+        </div>
+    
+)
+
+
+
+export default HomePage;
