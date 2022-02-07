351b0397c8b3d52d8f6dfa628f3f6c15840968d4
Remigiusz Mikolajewicz <r.mikolajewicz@interia.pl>
2022-02-07 23:06:51 +0100

    started inplementing routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
+
 import './App.css';
 import HomePage from './pages/homepage/HomePage';
+import {Route, Switch} from 'react-router-dom';
 
 
-
-
+const HatsPage = () => (
+  <h1>Hats Page</h1>
+)
 
 function App() {
   return (
-    <HomePage></HomePage>
+    <div>
+    <Switch>
+      <Route  path='/' component={HomePage}/>
+      <Route  path='/hats' component={HatsPage}/>
+    </Switch>
+
+    </div>
   );
 }
 
