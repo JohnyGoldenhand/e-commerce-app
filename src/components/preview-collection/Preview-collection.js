9e1390d61b39dc5d4f9d60e5a0a21578bbadaf25
Remigiusz Mikolajewicz <r.mikolajewicz@interia.pl>
2022-02-14 16:11:44 +0100

    updating shop page with collection preview component and collection item component

--- src/components/preview-collection/Preview-collection.js
+++ src/components/preview-collection/Preview-collection.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import './Preview-collection.scss';
-
+import CollectionItem from '../collection-item/Collection-item';
 
 const PreviewCollection = ({title, items}) => {
   return (
-    <div>
+    <div className='collection-preview'>
         <h1 className='title'>
             {title.toUpperCase()}
         </h1>
         <div className='preview'>
             {
-                items.filter((item, idx) => idx < 4).map(item => (
-                    <div key = {item.id}>{item.name}</div>
+                items
+                .filter((item, idx) => idx < 4)
+                .map(({id, ...otherItemProps})=> (
+                    <CollectionItem key = {id} {...otherItemProps} />
                 ))
             }
         </div>
