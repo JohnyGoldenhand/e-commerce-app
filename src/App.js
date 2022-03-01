23c8098aefed891ccbda425dafdce4522c42ad06
Remigiusz Mikolajewicz <r.mikolajewicz@interia.pl>
2022-03-01 18:25:10 +0100

    styled sign in buttons with dynamic class names

--- src/App.js
+++ src/App.js
@@ -5,19 +5,46 @@ import {Route, Switch} from 'react-router-dom';
 import ShopPage from './pages/ShopPage/ShopPage';
 import Header from './components/header/header'
 import SignInPage from './pages/SignInPage/SignInPage'
+import {auth} from './firebase/firebase.utils';
+import React from 'react';
 
-function App() {
-  return (
-    <div>
-    <Header/>
-    <Switch>
-      <Route  exact path='/' component={HomePage}/>
-      <Route  path='/shop' component={ShopPage}/>
-      <Route  path='/signin' component={SignInPage}/>
-    </Switch>
+class App extends React.Component {
+  constructor(){
+    super();
 
-    </div>
-  );
+    this.state = {
+      currentUser: null
+    }
+  }
+
+  unsubscribeFromAuth = null;
+
+  componentDidMount(){
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({currentUser: user});
+
+      console.log(user);
+    })
+  }
+
+  componentWillUnmount(){
+    this.unsubscribeFromAuth()
+  }
+
+
+  render(){
+    return (
+      <div>
+      <Header currentUser={this.state.currentUser}></Header>
+      <Switch>
+        <Route  exact path='/' component={HomePage}/>
+        <Route  path='/shop' component={ShopPage}/>
+        <Route  path='/signin' component={SignInPage}/>
+      </Switch>
+  
+      </div>
+    );
+  }
 }
 
 export default App;
